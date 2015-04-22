8d6dde1f4358e62d7a29111511a6e9fb2c3e5ae4
Philip Barker <philipbarker1012@googlemail.com>
2015-04-22 14:57:55 +0100

    Chamber movement edits
    - Distance from one chamber to another
    - Stared to work on defining the direction in which the chamber is
    moving

--- Assets/Engine/Components/ChamberMove.cs
+++ Assets/Engine/Components/ChamberMove.cs
@@ -5,41 +5,59 @@ public class ChamberMove : MonoBehaviour {
 	
 	Ray2D ray;
 	RaycastHit2D rayHit;
+
+	private Vector2 initPos;
 		
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetMouseButton (0)) 
+
+		///
+		// When the mouse button is first pressed down
+		///
+		if (Input.GetMouseButtonDown (0)) 
 		{
-
-
+			// get mouse position and 2d raycast hit
 			Vector2 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			RaycastHit2D hit = Physics2D.Raycast (mousePos, Vector2.zero);
+			
 			if (hit.collider != null) 
 			{
 				if (hit.collider.tag == "Chamber")
 				{
-					Vector2 initPos = hit.collider.gameObject.transform.position;
-					//hit.collider.gameObject.GetComponent<SpriteRenderer>().sprite = 
-					//Debug.Log (hit.collider.name);
-					Debug.Log(mousePos);
+					//define intial chamber position
+					initPos = hit.collider.gameObject.transform.position;
 
+				}
+			}
+		}
+
+		///
+		// If left mouse is down
+		///
+		if (Input.GetMouseButton (0)) 
+		{
+			// get mouse position and 2d raycast hit
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast (mousePos, Vector2.zero);
+
+			if (hit.collider != null) 
+			{
+				if (hit.collider.tag == "Chamber")
+				{
+					// define chamber position
 					Vector2 pos = hit.collider.gameObject.transform.position;
 
+					// round the postion so it is only moves by one unit
 					pos.x = Mathf.Round(mousePos.x);
 					pos.y = Mathf.Round(mousePos.y);
-
-					Vector2 RoundedPos = Helpers.RoundVec2(pos);
-
+					// move the chamber with the new round positions
 					hit.collider.gameObject.transform.position = new Vector2( pos.x , pos.y );
 
-					foreach (Chamber cham in CreateChamberArray.level.LevelChambers)
-					{
+					// determine which direction the chamber is being moved
+					float differencePos = Vector2.Distance (initPos, pos);
 
-
-					}
-
-				//	Debug.Log(pos.x);
+					Debug.Log("the difference : " + differencePos);
 
 
 				}
@@ -47,11 +65,14 @@ public class ChamberMove : MonoBehaviour {
 			}
 		}
 
+		///
+		// if lef mouse Button is up
+		///
 		if (Input.GetMouseButtonUp (0)) 
 		{
 
 
 		}
 	}
-
+	
 }
