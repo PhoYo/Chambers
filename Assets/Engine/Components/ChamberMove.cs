6bbef1f76f723765245c8019469fd69395c0aa2f
Philip Barker <philipbarker1012@googlemail.com>
2015-04-28 17:14:37 +0100

    Started to write grid update
    - Grid update (started) working on the logic and considering game
    design.

--- Assets/Engine/Components/ChamberMove.cs
+++ Assets/Engine/Components/ChamberMove.cs
@@ -6,15 +6,20 @@ public class ChamberMove : MonoBehaviour {
 	Ray2D ray;
 	RaycastHit2D rayHit;
 
+	public Transform LevelObject;
+
 	private Vector2 initPos;
+	private Vector2 pos;
 
-	private enum MovementDir {
-
+	private enum MovementDir 
+	{
 		Left,
 		Right,
 		Up,
-		Down
+		Down,
+		current
 	}
+
 	private MovementDir movementDir;
 
 
@@ -23,9 +28,9 @@ public class ChamberMove : MonoBehaviour {
 	void Update () 
 	{
 
-		///
+		///////////////////////////////////////////////////////////////////////////////////
 		// When the mouse button is first pressed down
-		///
+		///////////////////////////////////////////////////////////////////////////////////
 		if (Input.GetMouseButtonDown (0)) 
 		{
 			// get mouse position and 2d raycast hit
@@ -39,13 +44,18 @@ public class ChamberMove : MonoBehaviour {
 					//define intial chamber position
 					initPos = hit.collider.gameObject.transform.position;
 
+					initPos.x = Mathf.Round(initPos.x);
+					initPos.y = Mathf.Round(initPos.y);
+
+					Debug.Log(" inital position : x :" + initPos.x + " y " + initPos.y);
+
 				}
 			}
 		}
 
-		///
+		///////////////////////////////////////////////////////////////////////////////////
 		// If left mouse is down
-		///
+		///////////////////////////////////////////////////////////////////////////////////
 		if (Input.GetMouseButton (0)) 
 		{
 			// get mouse position and 2d raycast hit
@@ -58,64 +68,36 @@ public class ChamberMove : MonoBehaviour {
 				{
 
 					// define chamber position
-					Vector2 pos = hit.collider.gameObject.transform.position;
+					pos = hit.collider.gameObject.transform.position;
 					// round the postion so it is only moves by one unit
-					pos.x = Mathf.Round(mousePos.x);
-					pos.y = Mathf.Round(mousePos.y);
+					pos.x = Mathf.Round(pos.x);
+					pos.y = Mathf.Round(pos.y);
 
-					// determine which direction the chamber is being moved
-					if (pos.x > initPos.x) {
+
+					///////////////
+					// DIRECTION //
+					///////////////
+					if (pos.x == initPos.x && pos.y == initPos.y) {
+						movementDir = MovementDir.current;
+					} else if (pos.x  > initPos.x) {
 						movementDir = MovementDir.Right;
-					} else if ( pos.x < initPos.x) {
+					} else if ( pos.x  < initPos.x) {
 						movementDir = MovementDir.Left;
-					} else if (pos.y > initPos.y) {
+					} else if (pos.y  > initPos.y ) {
 						movementDir = MovementDir.Up;
-					} else if (pos.y < initPos.y) {
+					} else if (pos.y  < initPos.y ) {
 						movementDir = MovementDir.Down;
 					}
 
 					// find out the distance that the chamber has moved
 					float differencePos = Vector2.Distance (initPos, pos);
 
-					// go through all of the chambers in the array and move those that should be effected
-					//for (int x = 0;  x < 8; x++)
-					//{
-					//	for (int y = 0; y < 8; y++)
-					//	{
-						//	if (y == pos.y)
-						//	{
-								// move left
-								if (movementDir == MovementDir.Left){
-									//debug
-									Debug.Log("left");
-									//move chambers method
-									moveChambers(MovementDir.Left, pos, differencePos, true);
-								} else if (movementDir == MovementDir.Right) {
-									//debug
-									Debug.Log("right");
-									//move chambers
-									moveChambers(MovementDir.Right, pos, differencePos, false);
-								} else if (movementDir == MovementDir.Up) {
-									Debug.Log("up");
-									//move chambers
-									moveChambers(MovementDir.Up, pos, differencePos, true);
+			
 
-								} else if (movementDir == MovementDir.Down) {
-									Debug.Log("down");
-									//move chambers
-									moveChambers(MovementDir.Down, pos, differencePos, false);
-								}
 
 							//}
 						//}
 				//	}
-
-
-
-
-
-
-
 
 					// move the chamber with the new round position
 					//hit.collider.gameObject.transform.position = new Vector2( pos.x , pos.y );
@@ -124,28 +106,61 @@ public class ChamberMove : MonoBehaviour {
 			}
 		}
 
-		///
+		///////////////////////////////////////////////////////////////////////////////////
 		// if left mouse Button is up
-		///
+		///////////////////////////////////////////////////////////////////////////////////
 		if (Input.GetMouseButtonUp (0)) 
 		{
-			// update the chamber array with the new content
 
-			// check if the chambers fall inside the bounds defined.
-
-			// Find the null locations in the chamber array and fill with new chambers
-
-
-			// 
-
+			// get mouse position and 2d raycast hit
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast (mousePos, Vector2.zero);
+			
+			if (hit.collider != null) 
+			{
+				if (hit.collider.tag == "Chamber")
+				{
+					// update the chamber array with the new content
+					// move left
+					if (movementDir == MovementDir.Left){
+						//debug
+						Debug.Log("left");
+						//move chambers method
+						moveChambers(MovementDir.Left, pos, true);
+					} else if (movementDir == MovementDir.Right) {
+						//debug
+						Debug.Log("right");
+						//move chambers
+						moveChambers(MovementDir.Right, pos, false);
+					} else if (movementDir == MovementDir.Up) {
+						Debug.Log("up");
+						//move chambers
+						moveChambers(MovementDir.Up, pos, true);
+						
+					} else if (movementDir == MovementDir.Down) {
+						Debug.Log("down");
+						//move chambers
+						moveChambers(MovementDir.Down, pos, false);
+					}
+					// check if the chambers fall inside the bounds defined.
+					// Find the null locations in the chamber array and fill with new chambers
+				}
+			}
 		}
+	
+
+
 	
 	}
 
-
-	void moveChambers(MovementDir direction, Vector2 currentChamber, float movementAmount, bool positive)
+	/// <summary>
+	/// Moves the chambers.
+	/// </summary>
+	/// <param name="direction">Direction.</param>
+	/// <param name="currentChamber">Current chamber.</param>
+	/// <param name="positive">If set to <c>true</c> positive.</param>
+	void moveChambers(MovementDir direction, Vector2 currentChamber, bool positive)
 	{
-
 		for (int x = 0; x < 8; x++) 
 		{
 			for (int y = 0; y < 8; y++) 
@@ -162,9 +177,9 @@ public class ChamberMove : MonoBehaviour {
 					if (currentChamber.y == y)
 					{
 						if (positive){
-							changedPos = ChambersToMove.position.x - movementAmount;
+							changedPos = ChambersToMove.position.x - 1;
 						} else {
-							changedPos = ChambersToMove.position.x + movementAmount;
+							changedPos = ChambersToMove.position.x + 1;
 						}
 
 						// and move the chambers
@@ -174,9 +189,9 @@ public class ChamberMove : MonoBehaviour {
 					if (currentChamber.x == x)
 					{
 						if (positive){
-							changedPos = ChambersToMove.position.y + movementAmount;
+							changedPos = ChambersToMove.position.y + 1;
 						} else {
-							changedPos = ChambersToMove.position.y - movementAmount;
+							changedPos = ChambersToMove.position.y - 1;
 						}
 						// and move the chambers
 						ChambersToMove.position = new Vector2(currentChamber.x, changedPos);
@@ -185,5 +200,50 @@ public class ChamberMove : MonoBehaviour {
 			}
 
 		}
+	}
+
+	/// <summary>
+	/// Updates the grid.
+	/// </summary>
+	void UpdateGrid (Transform[] changedItems)
+	{
+		// remove all old children from the grid that have changed
+		for (int x = 0; x < 8; x++) {
+			for (int y = 0; y < 8; y++) {
+				// if the chamber in the array has value (it could be empty....)
+				if (CreateChamberArray.ChamberArray[x,y] != null){
+
+					// check if the chamber has been defined to change
+
+					// make the value null
+
+				}
+			}
+		}
+
+
+		// work though all the children of the level gameobject.. 
+		foreach (Transform child in LevelObject)
+		{
+			// if statement to define if its one of the chambers that has changed postion
+
+
+			// round the positions of the 
+			float PosX  = Mathf.Round(child.position.x);
+			float PosY  = Mathf.Round(child.position.y);
+			// vector 2 for 
+			Vector2 v = new Vector2(PosX,PosY);
+			
+			// make the chamber positon the chamber number in the array
+			CreateChamberArray.ChamberArray[(int)v.x,(int)v.y].Object = child;
+			
+
+		}
+
+
+
+		// add the childern to the grid 
+
+
 	}
 }
